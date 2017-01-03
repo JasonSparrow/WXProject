fef5bbbe9e5815660229e477cc9d3f71c1dc08f9
小飞hub <2678199009@qq.com>
2017-01-04 00:36:35 +0800

    将业务数据分离到单独的文件中,使用require加载js文件

---
+++ demo1/data/post-data.js
@@ -0,0 +1,60 @@
+var load_data = [
+  {
+    date: "sep",
+    title: "浅谈小程序",
+    imageSrc: "/images/4.jpg",
+    avatar: "/images/avatar.jpg",
+    content: "最近这几天，相信不少人都被微信即将推出的“小程序”刷爆了朋友圈。继订阅号、服务号和企业号之后，微信终于为我们带来了又一重磅功能。有些微妙的是，微信“小程序”总给人一种似曾相识的感觉---如果你熟知Firefox OS、Chrome OS、YUNOS，亦或“轻应用”、H5的话。",
+    reading: "100",
+    collection: "90",
+  },
+  {
+    date: "sep",
+    title: "浅谈小程序",
+    imageSrc: "/images/5.jpg",
+    avatar: "/images/avatar1.jpg",
+    content: "最近这几天，相信不少人都被微信即将推出的“小程序”刷爆了朋友圈。继订阅号、服务号和企业号之后，微信终于为我们带来了又一重磅功能。有些微妙的是，微信“小程序”总给人一种似曾相识的感觉---如果你熟知Firefox OS、Chrome OS、YUNOS，亦或“轻应用”、H5的话。",
+    reading: "130",
+    collection: "80",
+  },
+  {
+    date: "sep",
+    title: "浅谈小程序",
+    imageSrc: "/images/4.jpg",
+    avatar: "/images/avatar.jpg",
+    content: "最近这几天，相信不少人都被微信即将推出的“小程序”刷爆了朋友圈。继订阅号、服务号和企业号之后，微信终于为我们带来了又一重磅功能。有些微妙的是，微信“小程序”总给人一种似曾相识的感觉---如果你熟知Firefox OS、Chrome OS、YUNOS，亦或“轻应用”、H5的话。",
+    reading: "100",
+    collection: "90",
+  },
+  {
+    date: "sep",
+    title: "浅谈小程序",
+    imageSrc: "/images/5.jpg",
+    avatar: "/images/avatar1.jpg",
+    content: "最近这几天，相信不少人都被微信即将推出的“小程序”刷爆了朋友圈。继订阅号、服务号和企业号之后，微信终于为我们带来了又一重磅功能。有些微妙的是，微信“小程序”总给人一种似曾相识的感觉---如果你熟知Firefox OS、Chrome OS、YUNOS，亦或“轻应用”、H5的话。",
+    reading: "130",
+    collection: "80",
+  },
+  {
+    date: "sep",
+    title: "浅谈小程序",
+    imageSrc: "/images/4.jpg",
+    avatar: "/images/avatar.jpg",
+    content: "最近这几天，相信不少人都被微信即将推出的“小程序”刷爆了朋友圈。继订阅号、服务号和企业号之后，微信终于为我们带来了又一重磅功能。有些微妙的是，微信“小程序”总给人一种似曾相识的感觉---如果你熟知Firefox OS、Chrome OS、YUNOS，亦或“轻应用”、H5的话。",
+    reading: "100",
+    collection: "90",
+  },
+  {
+    date: "sep",
+    title: "浅谈小程序",
+    imageSrc: "/images/5.jpg",
+    avatar: "/images/avatar1.jpg",
+    content: "最近这几天，相信不少人都被微信即将推出的“小程序”刷爆了朋友圈。继订阅号、服务号和企业号之后，微信终于为我们带来了又一重磅功能。有些微妙的是，微信“小程序”总给人一种似曾相识的感觉---如果你熟知Firefox OS、Chrome OS、YUNOS，亦或“轻应用”、H5的话。",
+    reading: "130",
+    collection: "80",
+  }
+]
+
+module.exports = {
+  postList: load_data
+}
