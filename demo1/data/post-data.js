e041d31a4a4c8bc443c0075332b628c326bf68b8
小飞hub <2678199009@qq.com>
2017-01-04 22:57:19 +0800

    页面跳转

--- demo1/data/post-data.js
+++ demo1/data/post-data.js
@@ -7,6 +7,7 @@ var load_data = [
     content: "最近这几天，相信不少人都被微信即将推出的“小程序”刷爆了朋友圈。继订阅号、服务号和企业号之后，微信终于为我们带来了又一重磅功能。有些微妙的是，微信“小程序”总给人一种似曾相识的感觉---如果你熟知Firefox OS、Chrome OS、YUNOS，亦或“轻应用”、H5的话。",
     reading: "100",
     collection: "90",
+    postId:0
   },
   {
     date: "sep",
@@ -16,6 +17,7 @@ var load_data = [
     content: "最近这几天，相信不少人都被微信即将推出的“小程序”刷爆了朋友圈。继订阅号、服务号和企业号之后，微信终于为我们带来了又一重磅功能。有些微妙的是，微信“小程序”总给人一种似曾相识的感觉---如果你熟知Firefox OS、Chrome OS、YUNOS，亦或“轻应用”、H5的话。",
     reading: "130",
     collection: "80",
+    postId:1
   },
   {
     date: "sep",
@@ -25,6 +27,7 @@ var load_data = [
     content: "最近这几天，相信不少人都被微信即将推出的“小程序”刷爆了朋友圈。继订阅号、服务号和企业号之后，微信终于为我们带来了又一重磅功能。有些微妙的是，微信“小程序”总给人一种似曾相识的感觉---如果你熟知Firefox OS、Chrome OS、YUNOS，亦或“轻应用”、H5的话。",
     reading: "100",
     collection: "90",
+    postId:2
   },
   {
     date: "sep",
@@ -34,6 +37,7 @@ var load_data = [
     content: "最近这几天，相信不少人都被微信即将推出的“小程序”刷爆了朋友圈。继订阅号、服务号和企业号之后，微信终于为我们带来了又一重磅功能。有些微妙的是，微信“小程序”总给人一种似曾相识的感觉---如果你熟知Firefox OS、Chrome OS、YUNOS，亦或“轻应用”、H5的话。",
     reading: "130",
     collection: "80",
+    postId:3
   },
   {
     date: "sep",
@@ -43,6 +47,7 @@ var load_data = [
     content: "最近这几天，相信不少人都被微信即将推出的“小程序”刷爆了朋友圈。继订阅号、服务号和企业号之后，微信终于为我们带来了又一重磅功能。有些微妙的是，微信“小程序”总给人一种似曾相识的感觉---如果你熟知Firefox OS、Chrome OS、YUNOS，亦或“轻应用”、H5的话。",
     reading: "100",
     collection: "90",
+    postId:4
   },
   {
     date: "sep",
@@ -52,6 +57,7 @@ var load_data = [
     content: "最近这几天，相信不少人都被微信即将推出的“小程序”刷爆了朋友圈。继订阅号、服务号和企业号之后，微信终于为我们带来了又一重磅功能。有些微妙的是，微信“小程序”总给人一种似曾相识的感觉---如果你熟知Firefox OS、Chrome OS、YUNOS，亦或“轻应用”、H5的话。",
     reading: "130",
     collection: "80",
+    postId:5
   }
 ]
 
--- demo1/pages/posts/post.js
+++ demo1/pages/posts/post.js
@@ -12,36 +12,11 @@ Page({
         postKey:postData.postList
       });
   },
-  onReady:function(){
-    // 生命周期函数--监听页面初次渲染完成
-    console.log("onReady");
-  },
-  onShow:function(){
-    // 生命周期函数--监听页面显示
-    console.log("onShow");
-  },
-  onHide:function(){
-    // 生命周期函数--监听页面隐藏
-    console.log("onHide");
-  },
-  onUnload:function(){
-    // 生命周期函数--监听页面卸载
-    console.log("onUnload");
-  },
-  onPullDownRefresh: function() {
-    // 页面相关事件处理函数--监听用户下拉动作
-    console.log("onPullDownRefresh");
-  },
-  onReachBottom: function() {
-    // 页面上拉触底事件的处理函数
-    console.log("onReachBottom");
-  },
-  onShareAppMessage: function() {
-    // 用户点击右上角分享
-    return {
-      title: 'title', // 分享标题
-      desc: 'desc', // 分享描述
-      path: 'path' // 分享路径
-    }
+  onPostTap:function(event){
+    var postId = event.currentTarget.dataset.postid;
+    console.log(postId);
+    wx.navigateTo({
+      url: 'post-detail/post-detail',
+    })
   }
 })
